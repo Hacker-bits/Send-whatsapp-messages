09500a1d07c08c1bc1b77708d3d9be46ab805a4f
Hacker-bits <76698487+Hacker-bits@users.noreply.github.com>
2021-01-01 14:56:21 +0530

    Add files via upload

---
+++ send whatsapp messages.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  1 14:50:19 2021
+
+@author: DELL
+"""
+
+import pywhatkit as whatsapp
+whatsapp.sendwhatmsg("+91XXXXXXXXXX" ,"message", time-hour, time-minute)
+#Enter the phone number , message and time you want to send the message
+#You must first be logged on whatsapp-web
